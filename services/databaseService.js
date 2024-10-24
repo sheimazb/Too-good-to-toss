82fc18f3aedadd565926ecd2e8f7af4a6e153a79
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2024-10-24 12:48:08 +0100

    change the databaseService collection name

--- services/databaseService.js
+++ services/databaseService.js
@@ -2,7 +2,6 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../config/firebase";
 
 export const getAnnoncments = async () => {
-  const snapshot = await getDocs(collection(db, "Annoncments"));
+  const snapshot = await getDocs(collection(db, "Announcements"));
   return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 };
-
